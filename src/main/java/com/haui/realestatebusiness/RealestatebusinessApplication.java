7e0c1a045e9000ba0b75180f5162dec5089c9977
pogreen <trandaogrey@gmail.com>
2021-03-04 15:15:35 +0700

    base project

---
+++ src/main/java/com/haui/realestatebusiness/RealestatebusinessApplication.java
@@ -0,0 +1,13 @@
+package com.haui.realestatebusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestatebusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealestatebusinessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haui/realestatebusiness/RealestatebusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haui.realestatebusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestatebusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
